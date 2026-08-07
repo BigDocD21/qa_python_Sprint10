862e65cca5c36fbe4d3dfade9814c88abdf339a7
Dmitriy Varchenko <varchenko.dmitriy@bk.ru>
2026-08-07 17:28:24 +0100

    Переписал код и попытался исправить свои ошибки (else if и попытался сделать теты более атомарными)

--- test_pracktic.py
+++ test_pracktic.py
@@ -14,143 +14,157 @@ class TestBooksCollector:
 
         assert len(collector.get_books_genre()) == 2
 
-    @pytest.mark.parametrize("book_name, should_be_added", [
-        ('A' * 40, True),
-        ('Териминатор', True), 
-        ('A' * 41, False),
-        ('', False),
-        ('A' * 100, False),
+    @pytest.mark.parametrize("book_name, expected_count", [
+        ('A' * 40, 1),
+        ('Териминатор', 1),
+        ('A' * 41, 0),
+        ('', 0),
+        ('A' * 100, 0),
     ])
-    def test_add_new_book_length_validation(self, book_name, should_be_added):
-        collector = BooksCollector()
-        collector.add_new_book(book_name)
-        
-        books_dict = collector.get_books_genre()
-        current_names = list(books_dict.keys())
-        
-        if should_be_added:
-            assert len(books_dict) == 1
-            assert book_name in current_names
-        else:
-            assert len(books_dict) == 0
-            assert book_name not in current_names
 
-    @pytest.mark.parametrize("name1, name2, expected_count", [
-        ("Геральт", "Геральт", 1),
-        ("Геральт", "1976", 2),
-        ("A" * 40, "A" * 40, 1),
-    ])
-    def test_add_new_book_handles_duplicates(self, name1, name2, expected_count):
+
+    def test_add_new_book_length_validation(self, book_name, expected_count):
         collector = BooksCollector()
-        
+
+        collector.add_new_book(book_name)
+        books_dict = collector.get_books_genre()
+
+        assert len(books_dict) == expected_count
+
+
+    def test_add_new_book_rejects_duplicate(self):
+        collector = BooksCollector()
+
+        book_name = "Геральт"
+
+        collector.add_new_book(book_name)
+        collector.add_new_book(book_name)
+
+        books_dict = collector.get_books_genre()
+
+        assert len(books_dict) == 1
+        assert book_name in books_dict.keys()
+
+
+    def test_add_new_book_adds_different_books(self):
+        collector = BooksCollector()
+
+        name1 = "Геральт"
+        name2 = "Йеннифер"
+
         collector.add_new_book(name1)
         collector.add_new_book(name2)
-        
+
         books_dict = collector.get_books_genre()
-        current_names = list(books_dict.keys())
-        
-        assert len(books_dict) == expected_count
-        
-        if name1 == name2:
-            assert current_names.count(name1) == 1
-        else:
-            assert name1 in current_names
-            assert name2 in current_names 
+
+        assert len(books_dict) == 2
+        assert name1 in books_dict.keys()
+        assert name2 in books_dict.keys()
 
 
-    def test_set_genre_and_find_by_genre(self):
+    def test_set_book_genre_updates_genre(self):
         collector = BooksCollector()
+
         book_name = "Песнь льда и пламени"
         genre = "Фантастика"
-        
+
         collector.add_new_book(book_name)
         collector.set_book_genre(book_name, genre)
-        
+
         assert collector.get_book_genre(book_name) == genre
-        
-        books_in_genre = collector.get_books_with_specific_genre(genre)
-        assert book_name in books_in_genre
-        
+
+
+    def test_get_books_with_specific_genre_filters_correctly(self):
+        collector = BooksCollector()
+
+        book_name = "Песнь льда и пламени"
+        genre = "Фантастика"
         other_genre = "Ужасы"
+
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, genre)
+
+        books_in_genre = collector.get_books_with_specific_genre(genre)
         books_in_other_genre = collector.get_books_with_specific_genre(other_genre)
-        assert book_name not in books_in_other_genre 
+
+        assert book_name in books_in_genre
+        assert book_name not in books_in_other_genre
 
 
     @pytest.mark.parametrize("restricted_genre", ['Ужасы', 'Детективы'])
+
+
     def test_get_books_for_children_excludes_restricted_genres(self, restricted_genre):
         collector = BooksCollector()
+
         book_name = "Воспламеняющая взглядом"
-        
+
         collector.add_new_book(book_name)
         collector.set_book_genre(book_name, restricted_genre)
-        
+
         children_books = collector.get_books_for_children()
-        
-        assert book_name not in children_books, (
-            f"Ошибка! Книга '{book_name}' с жанром '{restricted_genre}' "
-            f"попала в подборку для детей."
-        ) 
+
+        assert book_name not in children_books
 
 
     def test_add_and_get_favorites(self):
         collector = BooksCollector()
-        book_name = "Ведьмак"
-        
-        collector.add_new_book(book_name)
-        
 
+        book_name = "Ведьмак"
+
+        collector.add_new_book(book_name)
         collector.add_book_in_favorites(book_name)
-        
 
         favorites = collector.get_list_of_favorites_books()
-        
 
         assert book_name in favorites
         assert len(favorites) == 1
 
-    def test_delete_from_favorites(self):
 
+    def test_delete_from_favorites(self):
         collector = BooksCollector()
+
         book_name = "Владычица Озер"
-        
+
         collector.add_new_book(book_name)
         collector.add_book_in_favorites(book_name)
-        
-
         collector.delete_book_from_favorites(book_name)
-        
+
         favorites = collector.get_list_of_favorites_books()
-        
+
         assert book_name not in favorites
         assert len(favorites) == 0
-        
+
         collector.delete_book_from_favorites("Не существующая книга")
 
         assert len(favorites) == 0
 
     def test_new_book_has_no_genre_initially(self):
         collector = BooksCollector()
+
         book_name = "Новая книга"
-        
+
         collector.add_new_book(book_name)
-        
         genre = collector.get_book_genre(book_name)
-        
-        assert genre == '', f"Ожидался пустой жанр сразу после добавления, а получено: '{genre}'"
+
+        assert genre == ''
+
 
     def test_add_book_in_favorites_unique(self):
         collector = BooksCollector()
+
         book_name = "Ведьмак"
-        
+
         collector.add_new_book(book_name)
-        
         collector.add_book_in_favorites(book_name)
+
         favorites_first = collector.get_list_of_favorites_books()
+
         assert len(favorites_first) == 1
         
         collector.add_book_in_favorites(book_name)
+
         favorites_second = collector.get_list_of_favorites_books()
         
         assert len(favorites_second) == 1
-        assert favorites_first == favorites_second
-        
+        assert favorites_first == favorites_second 
